eec346dd7ae5533ab55a2055d28dd1f8016cda01
TheKvc <karanveerchouhan@gmail.com>
2021-06-18 13:03:33 +0530

    Init Commit

---
+++ Demo_ReadLine.bat
@@ -0,0 +1,36 @@
+@echo off
+SetLocal Enabledelayedexpansion
+cls
+
+REM Reading Name of file... for Title bar
+For /f "tokens=1,2* delims=_" %%A in ("%~n0") do (Set "_Title=%%~B")
+
+Title !_Title! Demo File - www.thebateam.org
+Cd Files
+Echo.
+Echo ----Current Data in Demo_File.txt--------
+Echo.
+Type "Demo_File.txt"
+Echo.
+Echo -----------------------------------------
+
+Set _Counter=0
+REM Getting Current number of lines in the text file
+For /f "tokens=*" %%A in ('Type "Demo_File.txt"') do (Set /A _Counter+=1)
+
+:GetLoc
+Set _Loc=
+Set /p "_Loc=Enter the Line to Read [Line Number]: "
+If not Defined _Loc (Goto :GetLoc)
+
+REM Checking if the entered value is number
+Set _Copy_Loc=%_Loc%
+For /L %%A in (0,1,9) do (Set "_Copy_Loc=!_Copy_Loc:%%~A=x!")
+
+Set "_Copy_Loc=!_Copy_Loc:x=!"
+
+IF /i "!_Copy_Loc!" NEQ "" (Goto :GetLoc)
+IF !_Loc! GTR !_Counter! (Goto :GetLoc)
+
+ReadLine "Demo_File.txt" "%_Loc%"
+pause >nul
---
+++ README.md
@@ -0,0 +1,12 @@
+# ReadLine
+**Description:**
+Reads a File and prints specific line on the console.
+
+AUTHOR:	**Kvc**
+
+# USAGE
+
+<code>Call ReadLine [File] [LineNumber]</code>
+
+​	Examples: 		ReadLine "Demo.txt" 5
+
